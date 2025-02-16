777e57be6ae7c553ddc9c17533f713be1dc65983
Frathol <michaelmosses35746@gmail.com>
2025-02-16 19:54:19 +0700

    Membenarkan fitur yang keliru

--- src/App.java
+++ src/App.java
@@ -8,7 +8,7 @@ public class App {
         System.out.print("Masukkan Angka : ");
         String angka = oke.nextLine();
 
-        for(int i = angka.length();i >= 0;i--){
+        for(int i = angka.length()-1;i >= 0;i--){
             wadah = wadah + angka.charAt(i);
         }
 
