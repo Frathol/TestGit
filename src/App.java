3a7d8cead08b346439a534cc6c911726f6c6c6f0
Frathol <michaelmosses35746@gmail.com>
2025-02-16 20:16:20 +0700

    Merubah Fitur Singkat

--- src/App.java
+++ src/App.java
@@ -2,14 +2,23 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        System.out.println("Oke!!!!!!!!!");
+
+        Thread.sleep(2000);
+
+        System.out.println("Coba Fitur Sederhana");
+        ubah1();
+    }
+
+    public static void ubah1(){
         Scanner oke = new Scanner(System.in);
 
         String wadah = "";
-        System.out.print("Masukkan Angka : ");
-        String angka = oke.nextLine();
+        System.out.print("Masukkan inputan : ");
+        String inputan = oke.nextLine();
 
-        for(int i = angka.length()-1;i >= 0;i--){
-            wadah = wadah + angka.charAt(i);
+        for(int i = inputan.length()-1;i >= 0;i--){
+            wadah = wadah + inputan.charAt(i);
         }
 
         System.out.println("Jika Dibalik : " + wadah);
