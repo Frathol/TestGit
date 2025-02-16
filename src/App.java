631eea8f0374c22a60fee7ad821c50972aa9c531
Frathol <michaelmosses35746@gmail.com>
2025-02-16 19:41:33 +0700

    Menambahkan fitur input dari user

---
+++ src/App.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner oke = new Scanner(System.in);
+
+        System.out.print("Masukkan Angka : ");
+        int angka = oke.nextInt();
+
+        oke.close();
+    }
+}
