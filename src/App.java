fff3ba789f6176301bce33742a82849a54af331d
Frathol <michaelmosses35746@gmail.com>
2025-02-16 19:52:49 +0700

    Menambahkan fitur Membalikkan inputan User

--- src/App.java
+++ src/App.java
@@ -4,8 +4,15 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner oke = new Scanner(System.in);
 
+        String wadah = "";
         System.out.print("Masukkan Angka : ");
-        int angka = oke.nextInt();
+        String angka = oke.nextLine();
+
+        for(int i = angka.length();i >= 0;i--){
+            wadah = wadah + angka.charAt(i);
+        }
+
+        System.out.println("Jika Dibalik : " + wadah);
 
         oke.close();
     }
